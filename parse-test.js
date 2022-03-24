b9ec4bd8e5d827100f3f90577525d1bedd5b5ed7
nikolai <mail@nikolai.fyi>
2022-03-24 11:07:57 -0400

    nesting molds

--- parse-test.js
+++ parse-test.js
@@ -10,6 +10,22 @@ it('parses', t => {
     t.equal(ast.children.length, 2)
 })
 
+it('nesting', t=>{
+    const ast = p(` ('a ('c .b)) -+ (.a) `)
+    t.equal(ast.type, 'rule')
+    //show(ast)
+
+    const ast2 = p(` ('a 'b ('c .d .e)) -+ (.e)`)
+    //show(ast2)
+    t.equal(ast.type, 'rule')
+})
+
+it('val in nested exp after var', t=>{
+    const ast = p(`  ('a .b ('c .d)) -+ (.d)  `)
+    show(ast)
+    t.equal(ast.type, 'rule')
+})
+
 it('no parse bad var/val org', t=>{
     const ast = p(`(.a 'b) -+ (.a)`)
     //show(ast)
--- parse.js
+++ parse.js
@@ -1,19 +1,32 @@
-const ebnf = require('ebnf')
-
-const gram = `
+_=`; { ws=implicit
+_='ws{implicit }"}
+=;_
+rule ::= (mold) RSEP (term) EOF
+mold ::= "(" mexp* ")"     {framgent=true}
+`;_=`
+;_= mexp ::= mold | vvl fragment
+=_;={ ws=implicit {=fragment true=}
+rule ::=
+`;_=`
 { ws=implicit }
-rule ::= (mold) RSEP (term)
-mold ::= "(" vvl ")"
+rule ::= (mold) RSEP (term) EOF
+
+mold ::= "(" mexp* ")"     {framgent=true}
+mexp ::= mold | vvl        {fragment=true}
+vvl  ::= val mexp | var*   {fragment=true}
+
+vvr  ::= (val | var)*      {fragment=true}
 term ::= "(" vvr ")"
+
 var  ::= "." sym
 val  ::= "'" sym
-vvl  ::= val vvl | var*
-vvr  ::= (val | var)*
-sym  ::= [a-z0-9] {fragment=true}
+sym  ::= [a-z0-9]          {fragment=true}
 RSEP ::= "-+"
 WS   ::= " "
 `
-const parser = new ebnf.Parser(ebnf.Grammars.Custom.getRules(gram), {})
+
+const ebnf = require('ebnf')
+const parser = new ebnf.Parser(ebnf.Grammars.Custom.getRules(_), {})
 const parse =s=> {
     const ast = parser.getAST(s)
     return ast
