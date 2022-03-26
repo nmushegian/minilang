c1fe0473b8600c5adb3f848ba2899eb6f8a04d42
nikolai <mail@nikolai.fyi>
2022-03-26 18:53:22 -0400

    sketch

--- ml.js
+++ ml.js
@@ -2,6 +2,7 @@ const it = require('tapzero').test
 const toss =s=> { throw new Error(s) }
 const need =(b,s)=> b ?? toss(s)
 const log = console.log
+const show =(o,d)=> JSON.stringify(o, null, d??2)
 
 const rule0 = ['RULE', ['if', '0', '.a', '.b'], '.a']
 const rule1 = ['RULE', ['if', '1', '.a', '.b'], '.b']
@@ -13,17 +14,39 @@ const term = ['if', '0', 'ali', 'bob']
 
 const isval =s=> (typeof(s) == 'string' && s[0] != '.')
 const isvar =s=> (typeof(s) == 'string' && s[0] == '.')
-const islist =l=> l instanceof Array
+const islist =l=> (l instanceof Array)
+
+it('isval isvar islist', t=>{
+    t.ok(isval('if'))
+    t.ok( ! isval('.if'))
+    t.ok(isvar('.if'))
+    t.ok( ! isvar('if'))
+    t.ok(islist([]))
+    t.ok( ! islist("str"))
+})
 
 const match =(patt,term,onlyvars)=> {
+    //log('match', patt, term, onlyvars)
+    if (isval(patt)) {
+        log('patt is val')
+        need(isval(term), `match fail: ${patt} is a val but ${term} is not`)
+        need(!onlyvars, `match fail: ${patt} is a val in only-vars tail`)
+        return {}
+    }
+    if (isvar(patt)) {
+        log('patt is var')
+        return {[patt]: term}
+    }
+    need(islist(patt), `match panic: malformed pattern`)
     if (patt.length == 0) {
         return {}
     }
-    need(term.length > 0, `match fail: term is empty but pattern is ${patt}`)
+    need(islist(term) && term.length > 0, `match fail: term is empty but pattern is ${patt}`)
     const p0 = patt[0]
     const t0 = term[0]
     const prest = patt.slice(1)
     const trest = term.slice(1)
+
     if (isval(p0)) {
         need(isval(t0), `match fail: ${p0} is a val but ${t0} is not`)
         need(!onlyvars, `match fail: ${p0} is a val in only-vars tail`)
@@ -68,22 +91,32 @@ const appr =(rule,term)=> {
 }
 
 const appl =(lang,term)=> {
-    for (const rule of lang) {
-        log(rule)
-    }
+    let next; do {
+        next = false;
+        //log('lang', lang)
+        for (let rule of lang) {
+            //log('rule', rule)
+            try {
+                log('term before', term)
+                term = appr(rule, term)
+                log('term after', term)
+                next = true
+                break
+            } catch (e) {
+                //log(e)
+                continue
+            }
+        }
+    } while (next)
+    return term
 }
 
 it('appl', t=>{
-    appl(lang,term)
+    const term2 = appl(lang,term)
+    t.ok(term2)
+    log(term2)
 })
 
-it('isval isvar islist', t=>{
-    t.ok(isval('if'))
-    t.ok( ! isval('.if'))
-    t.ok(isvar('.if'))
-    t.ok( ! isvar('if'))
-    t.ok(islist([]))
-})
 
 it('match', t=>{
     const bind = match(rule0[1], term)
